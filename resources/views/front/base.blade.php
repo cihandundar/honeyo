c8affb258a4d9783c44509178dbe4da128580809
cihandundar <94462152+cihandundar@users.noreply.github.com>
2025-04-05 00:57:16 +0300

    boostrap ve swiper js kütüphaneleri entegre edildi

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -43,7 +43,10 @@
 
     {{-- CSS Files --}}
     <link rel="stylesheet" href="{{ frontAsset('css/main.css') }}">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
     {!! setting('head-meta-kodlari') !!}
     {!! setting('google-head-kodlari') !!}
     {!! setting('google-remarketing-kodlari') !!}
@@ -69,6 +72,9 @@
 
     {{-- JS Files --}}
     <script type="module" src="{{ frontAsset('js/main.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
 
     {!! nonCache('mail_alerts') !!}
     {!! setting('footer-meta-kodlari') !!}
